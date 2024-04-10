45c4486532835010bb87bce5601567b13e60f096
juaca2004 <114953837+juaca2004@users.noreply.github.com>
2024-04-10 09:42:41 -0500

    Admin's login v1

---
+++ LoginAdmin/index.js
@@ -0,0 +1,6 @@
+const nameInput = document.getElementById('nameInput');
+const passwordInput = document.getElementById('passwordInput');
+const login_admin= document.getElementById('login_admin');
+const login_doctor = document-getElementById('login_doctor')
+
+
