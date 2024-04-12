28443c486e83b1091923a2dca7ec0948c41249e5
sergioflorez30 <sergiofflorez2005@gmail.com>
2024-04-11 19:42:39 -0500

    Pagina previa Agregar,Buscar, Eliminar Doctores

---
+++ AgregarEliminarBuscarDoctores/indexAEB.js
@@ -0,0 +1,12 @@
+function agregarDoctor() {
+    alert("Funcionalidad para agregar doctor aún no implementada.");
+  }
+  
+  function buscarDoctor() {
+    alert("Funcionalidad para buscar doctor aún no implementada.");
+  }
+  
+  function eliminarDoctor() {
+    alert("Funcionalidad para eliminar doctor aún no implementada.");
+  }
+  
