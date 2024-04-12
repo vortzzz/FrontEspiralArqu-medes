a49c58dbbe96ab0484de5318dfae52397726c129
sergioflorez30 <sergiofflorez2005@gmail.com>
2024-04-11 22:19:24 -0500

    Pagina de eliminacion de doctores

--- AgregarEliminarBuscarDoctores/indexAEB.js
+++ AgregarEliminarBuscarDoctores/indexAEB.js
@@ -1,3 +1,19 @@
+const logo = document.querySelector('.logo');
+const homeButton = document.querySelector('.button:nth-child(1)');
+const doctorsButton = document.querySelector('.button:nth-child(2)');
+const exitButton = document.querySelector('.button:nth-child(3)');
+const logoIcesi = document.querySelector('.logoIcesi');
+
+
+const container = document.querySelector('.container');
+const fundacionImage = document.querySelector('.fundacion-image');
+const agregarButton = document.querySelector('.buttons button:nth-child(1)');
+const buscarButton = document.querySelector('.buttons button:nth-child(2)');
+const eliminarButton = document.querySelector('.buttons button:nth-child(3)');
+
+
+
+
 function agregarDoctor() {
     alert("Funcionalidad para agregar doctor aún no implementada.");
   }
---
+++ EliminarDoctores/indexDelete.js
@@ -0,0 +1,16 @@
+// Variables para los elementos del sidebar
+const logo = document.querySelector('.logo');
+const homeButton = document.querySelector('.button:nth-child(1)');
+const doctorsButton = document.querySelector('.button:nth-child(2)');
+const exitButton = document.querySelector('.button:nth-child(3)');
+const logoIcesi = document.querySelector('.logoIcesi');
+
+// Variables para los elementos del container
+const container = document.querySelector('.container');
+const doctorImage = document.querySelector('.fundacion-image');
+const doctorInput = document.getElementById('doctorInput');
+const eliminarButton = document.querySelector('.buttons button');
+
+function eliminarDoctor() {
+    
+}
