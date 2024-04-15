a323d2d85eee7300281a97ee453cd4544b6b30e7
juaca2004 <114953837+juaca2004@users.noreply.github.com>
2024-04-15 15:27:20 -0500

    login complete

---
+++ PrincipalPageAdmin/index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button');
+
+
+button.addEventListener('click',mostrarMensaje);
+
+function mostrarMensaje() {        
+alert('Has hecho clic en el botón');
+}
