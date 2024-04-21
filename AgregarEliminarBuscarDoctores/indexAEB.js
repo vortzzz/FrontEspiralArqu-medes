cba0d4f736d2bd85f73e8a6e64bde370bea7c98d
juansex <jsbo2003@gmail.com>
2024-04-21 16:30:23 -0500

    Mostrar Doctor en la busqueda

--- AgregarEliminarBuscarDoctores/indexAEB.js
+++ AgregarEliminarBuscarDoctores/indexAEB.js
@@ -8,11 +8,8 @@ function agregarDoctor() {
   }
   
   function buscarDoctor() {
-<<<<<<< HEAD
       window.location.href = "../BuscarDoctores/paginaSearch.html";
-=======
-    alert("Funcionalidad para buscar doctor aún no implementada.");
->>>>>>> 4f7501e0a6af80a7c5aafcfcf52074d1fe94eb2e
+
   }
   
   function eliminarDoctor() {
